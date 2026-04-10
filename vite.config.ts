cbb780d6175f9948bd2135b8852289cf6e47879d
nehamjadhav <nehamjadhav@gmail.com>
2026-04-10 12:32:08 +0530

    local changes

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+  base: '/PAF-website/',  
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
